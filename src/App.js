ea4f7c2fd3ea5ed0b673684cc0a54d58e1893122
iamvibhu <vibhu@trgtdigital.com>
2022-05-02 12:42:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			drivers: [],
+			DataisLoaded: false
+		};
+	}
+
+	componentDidMount() {
+		fetch(
+			"http://localhost:8080/drivers")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					drivers: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, drivers } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> {
+				drivers.map((driver) => (
+				<ol key = { driver.carUid } >
+					Driver_Name: <b>{ driver.name }</b>,
+					Driver_Contact: <b>{ driver.contact }</b>,
+					Driver_Car: <b>{ driver.carUid }</b>
+				</ol>
+				))
+			}
+		</div>
+	);
+}
 }
 
 export default App;
